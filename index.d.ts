12375cff1a840155ce49ea06c6d17283df88b278
Krutoy242 <Krutoy242@gmail.com>
2021-12-28 15:50:28 +0300

    🅃 types WIP for using as lib

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+/**
+ *
+ * @param {{[key:string]: any}} _options
+ */
+export default function parseDebugLog(_options?: {
+    [key: string]: any;
+}): Promise<any>;
+export const getModLoadTimeTuples: any;
+//# sourceMappingURL=index.d.ts.map
