7dc3ea60d2189033a28a2be6e732c531b5041665
Krutoy242 <Krutoy242@gmail.com>
2025-05-08 16:14:04 +0300

    fix: truncating

--- scripts/nospaces.ts
+++ scripts/nospaces.ts
@@ -1,5 +1,7 @@
 import { readFileSync, writeFileSync } from 'node:fs'
 
 const template = readFileSync('src/template.hbs', 'utf8')
-const nospaces = template.replace(/<img src="([\s\S]+?)"\/>/g, m => m.replace(/\s*\n\s*/g, '%20'))
+const nospaces = template.replace(/<img src="([\s\S]+?)"\/>/g, m => m
+  .replace(/\s*\n\s*/g, ' ')
+  .replace(/\s{2,}/g, ' '))
 writeFileSync('src/template-nospaces.hbs', nospaces)
--- src/helpers.hbs.js
+++ src/helpers.hbs.js
@@ -5,9 +5,14 @@ module.exports = {
   padStart: (len, text) => String(text).padStart(len),
   padEnd: (len, text) => String(text).padEnd(len),
   filterTime: (arr, treshold) => arr.filter(o => o.time >= treshold),
-  truncate: (arr, len, note) => Array.isArray(arr)
-    ? [...arr.slice(0, len), note]
-    : Object.fromEntries(Object.entries(arr).slice(0, len)),
+  truncate: (record, len, note) => {
+    const trunc = Object.fromEntries(Object.entries(record).slice(0, len))
+
+    if (typeof note == 'string')
+      trunc[note] = Object.values(record).slice(len).reduce((a, v) => a + v)
+
+    return trunc
+  },
   num: (text) => {
     if (typeof text != 'string' && typeof text != 'number')
       return String(text)
